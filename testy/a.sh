31c59bcf00384d1d4182104a1d7232761eea37e4
ms <in1ck@icloud.com>
2020-11-06 00:21:34 +0100

    fix: sprawdzanie nieistniejących katalogów

--- testy/a.sh
+++ testy/a.sh
@@ -10,15 +10,21 @@ fi
 cd zadania
 index=5
 pracownik=""
+rola=""
 for i in $(cat $plik); do
 	if [[ $index == 5 ]]; then
 		pracownik=$i
 		index=0
 	fi
+	if [[ $index == 3 ]]; then
+		rola=$i
+	fi
 	if [[ $index == 4 ]]; then
 		if [[ $i == 'DBD' ]]; then
-			printf "kredyt" > ${pracownik}/z1.txt
-			printf "lokata" > ${pracownik}/z2.txt
+			if [[ rola == "obsługa" ]]; then
+				printf "kredyt" > ${pracownik}/z1.txt
+				printf "lokata" > ${pracownik}/z2.txt
+			fi
 		fi
 	fi
 	(( ++index ))
