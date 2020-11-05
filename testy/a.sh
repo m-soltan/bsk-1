899865af32f2d0d5856d68561230e22ba76851cb
ms <in1ck@icloud.com>
2020-11-05 23:44:42 +0100

    poprawiony test a

--- testy/a.sh
+++ testy/a.sh
@@ -9,10 +9,17 @@ fi
 
 cd zadania
 index=5
+pracownik=""
 for i in $(cat $plik); do
+	if [[ $index == 5 ]]; then
+		pracownik=$i
+		index=0
+	fi
 	if [[ $index == 4 ]]; then
-		printf "kredyt" > $i/z1.txt
-		printf "lokata" > $i/z2.txt
+		if [[ $i == 'DBD' ]]; then
+			printf "kredyt" > ${pracownik}/z1.txt
+			printf "lokata" > ${pracownik}/z2.txt
+		fi
 	fi
 	(( ++index ))
 done
