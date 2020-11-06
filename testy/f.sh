391403cd7d5200576e29833d57d43a17d1949abf
ms <in1ck@icloud.com>
2020-11-06 13:16:50 +0100

    fix: tworzenie plików w nieistniejącym katalogu

--- testy/f.sh
+++ testy/f.sh
@@ -5,12 +5,12 @@ if [[ $# != 1 ]]; then
 	exit
 fi
 
-for i in $(grep "lokata" -lR zadania/$1); do
+for i in $(grep "lokata" -lR zadania/$1 | xargs -L 1 basename); do
 	timestamp=$(date +%s)
-	printf "zrobione" > lokaty/${i}_${timestamp}.txt
+	printf "zrobione" > lokaty/${1}_${timestamp}.txt
 done
 
-for i in $(grep "kredyt" -lR zadania/$1); do
+for i in $(grep "kredyt" -lR zadania/$1 | xargs -L 1 basename); do
 	timestamp=$(date +%s)
 	printf "zrobione" > kredyty/${i}_${timestamp}.txt
 done
