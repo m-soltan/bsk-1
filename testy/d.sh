dbf66c1b97a3107931d52cf4f923c461ebcc2de7
ms <in1ck@icloud.com>
2020-11-06 12:08:50 +0100

    test d

---
+++ testy/d.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+plik=$1
+
+if [[ $# != 1 ]]; then
+	echo "użycie: $0 plik"
+	exit
+fi
+
+cd zadania
+index=5
+pracownik=""
+rola=""
+for i in $(cat $1); do
+	if [[ $index == 5 ]]; then
+		pracownik=$i
+		index=0
+	fi
+	if [[ $index == 3 ]]; then
+		rola=$i
+	fi
+	if [[ $index == 4 ]]; then
+		if [[ $i == "DBD" ]]; then
+			if [[ $rola == "obsługa" ]]; then
+				cat ${pracownik}/z1.txt >/dev/null
+			fi
+		fi
+	fi
+	(( ++index ))
+done
